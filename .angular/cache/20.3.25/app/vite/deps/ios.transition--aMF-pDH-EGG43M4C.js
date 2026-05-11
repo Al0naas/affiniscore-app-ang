fbc8f0955766b512551ee06cb49ee433e8a6a1d3
Alonso Robles <alo.robles@duocuc.cl>
2026-05-11 06:52:49 -0400

    se agregaron archivos environments

---
+++ .angular/cache/20.3.25/app/vite/deps/ios.transition--aMF-pDH-EGG43M4C.js
@@ -0,0 +1,14 @@
+import {
+  iosTransitionAnimation,
+  shadow
+} from "./chunk-FYJMC2EU.js";
+import "./chunk-QJKEIU7E.js";
+import "./chunk-V5SJTEP7.js";
+import "./chunk-GHFY5PYR.js";
+import "./chunk-DWC2MENN.js";
+import "./chunk-XA5ED5VY.js";
+import "./chunk-EGSMBJJY.js";
+export {
+  iosTransitionAnimation,
+  shadow
+};
